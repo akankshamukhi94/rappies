5d83734fd7357772db3fc407ed15c923fa5e4b53
akankshamukhi94 <139205813+akankshamukhi94@users.noreply.github.com>
2024-03-21 23:09:48 -0400

    Add files via upload

---
+++ js/songs.js
@@ -0,0 +1,268 @@
+/**
+ * songs.js
+ *
+ * The app's songs
+ */
+
+window.songs = [
+  /* TODO */
+  /* Songs */
+
+  {
+    songId: "SID-1234101",
+    artistId: "AID-12341",
+    title: "Rihayi",
+    year: "2023",
+    duration: 181,
+    url: "https://youtu.be/uTgsRbvLhUI",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234102",
+    artistId: "AID-12341",
+    title: "Hasti Rahe tu",
+    year: "2023",
+    duration: 190,
+    url: "https://youtu.be/UyoDdroSXXs",
+    explicit: false
+  },
+  {
+    songId: "SID-1234103",
+    artistId: "AID-12341",
+    title: "Jadugar",
+    year: "2023",
+    duration: 188,
+    url: "https://youtu.be/IOcGS4D1tM0",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234104",
+    artistId: "AID-12341",
+    title: "Sirhaana",
+    year: "2023",
+    duration: 278,
+    url: "https://youtu.be/SCz8Hq9gw8Q",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234105",
+    artistId: "AID-12341",
+    title: "Perfy",
+    year: "2023",
+    duration: 198,
+    url: "https://youtu.be/gBmDrUBugns",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234106",
+    artistId: "AID-12341",
+    title: "Cowboy",
+    year: "2024",
+    duration: 136,
+    url: "https://youtu.be/4UUC_dBlRuc",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234111",
+    artistId: "AID-12342",
+    title: "Wanna Know",
+    year: "2023",
+    duration: 207,
+    url: "https://youtu.be/YMrMbm0fFxo",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234112",
+    artistId: "AID-12342",
+    title: "Hola Amigo",
+    year: "2023",
+    duration: 242,
+    url: "https://youtu.be/De9VIp37CjY",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234113",
+    artistId: "AID-12342",
+    title: "Prathana",
+    year: "2023",
+    duration: 205,
+    url: "https://youtu.be/OKPAPX_fGVw",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234114",
+    artistId: "AID-12342",
+    title: "Kaha tak",
+    year: "2022",
+    duration: 216,
+    url: "https://youtu.be/lt8ZIrNEUL8",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234115",
+    artistId: "AID-12342",
+    title: "Fall off",
+    year: "2023",
+    duration: 164,
+    url: "https://youtu.be/lQy2e-aWTfY",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234116",
+    artistId: "AID-12342",
+    title: "Joota Japani",
+    year: "2023",
+    duration: 159,
+    url: "https://youtu.be/vR16Gu-Uq00",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234117",
+    artistId: "AID-12342",
+    title: "woh raat",
+    year: "2024",
+    duration: 193,
+    url: "https://youtu.be/bvAS1avuOpY",
+    explicit: false
+  },
+  {
+    songId: "SID-1234121",
+    artistId: "AID-12343",
+    title: "Haath Varthi",
+    year: "2023",
+    duration: 149,
+    url: "https://youtu.be/pg2tsJErYH4",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234122",
+    artistId: "AID-12343",
+    title: "Nusta paisa",
+    year: "2022",
+    duration: 222,
+    url: "https://youtu.be/s1TBzhmt1vU",
+    explicit: true
+  },
+
+  {
+    songId: "SID-1234123",
+    artistId: "AID-12343",
+    title: "Broke is a joke",
+    year: "2022",
+    duration: 215,
+    url: "https://youtu.be/YdIxAdTDpWU?list=OLAK5uy_kN2J17eqKDzIj5nahO0XNJ_n_2HTcJqcs",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234124",
+    artistId: "AID-12343",
+    title: "Meri zindagi",
+    year: "2022",
+    duration: 155,
+    url: "https://youtu.be/bCEMycv7MI4",
+    explicit: true
+  },
+
+  {
+    songId: "SID-1234131",
+    artistId: "AID-12344",
+    title: "8 Asle",
+    year: "2024",
+    duration: 161,
+    url: "https://youtu.be/0FnZO-U5oHo",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234132",
+    artistId: "AID-12344",
+    title: "God Father",
+    year: "2024",
+    duration: 121,
+    url: "https://youtu.be/EkN3WnCM9dU",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234133",
+    artistId: "AID-12344",
+    title: "Armed",
+    year: "2024",
+    duration: 124,
+    url: "https://youtu.be/ubm-ncLftaQ",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234134",
+    artistId: "AID-12344",
+    title: "Slowly",
+    year: "2024",
+    duration: 167,
+    url: "https://youtu.be/wzhZ9QLeeWs",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234141",
+    artistId: "AID-12345",
+    title: "Hisaab",
+    year: "2024",
+    duration: 227,
+    url: "https://youtu.be/vJvp7i5fLas",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234142",
+    artistId: "AID-12345",
+    title: "100 Million",
+    year: "2024",
+    duration: 186,
+    url: "https://youtu.be/0dXiVYq8HBc",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234143",
+    artistId: "AID-12345",
+    title: "4.10",
+    year: "2024",
+    duration: 169,
+    url: "https://youtu.be/dOoVFyKNynY",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234144",
+    artistId: "AID-12345",
+    title: "Akela",
+    year: "2023",
+    duration: 243,
+    url: "https://youtu.be/TgRKK-HHm-I",
+    explicit: false
+  },
+
+  {
+    songId: "SID-1234145",
+    artistId: "AID-12345",
+    title: "3.59 AM",
+    year: "2021",
+    duration: 286,
+    url: "https://youtu.be/eHyl9KG9RWA",
+    explicit: false
+  }
+];
